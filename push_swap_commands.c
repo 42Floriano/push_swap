15ba1cf3e0379439f62251896a647517f5fdd74b
Floriano <floriano.webdev@gmail.com>
2024-01-17 15:30:30 +0100

    push_swap function

--- push_swap_commands.c
+++ push_swap_commands.c
@@ -6,7 +6,7 @@
 /*   By: falberti <falberti@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/16 15:55:01 by falberti          #+#    #+#             */
-/*   Updated: 2024/01/16 16:11:07 by falberti         ###   ########.fr       */
+/*   Updated: 2024/01/17 15:29:57 by falberti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,22 +17,26 @@ void	push_swap(t_stack_node **a, t_stack_node **b)
 	t_stack_node	*smallest;
 	int				len_a;
 
-	len_a = stack_len(*a);
+	len_a = stack_len(a[0]);
 	if (len_a == 5)
 		handle_five(a, b);
 	else
 	{
-		while (len_a > 3)
-		{
+		while (len_a-- > 3)
 			pb(b, a);
-			len_a--;
-		}
 	}
 	tiny_sort(a);
 	while (b[0])
 	{
-		init_nodes(*a, *b);
+		init_nodes(a[0], b[0]);
 		move_nodes(a, b);
 	}
-	
+	set_current_position(a[0]);
+	smallest = find_smallest(a[0]);
+	if (smallest->above_median)
+		while (a[0] !+ smallest)
+			ra(a);
+	else
+		while (a[0] != smallest)
+			rra(a);
 }
